24ab4bdf34026ccedb8867278ba7a5000f7bc0e5
Johan Josefsson <johan.josefsson@rocketmail.com>
2018-03-27 20:55:26 +0200

    Everyting works in principle

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # surodel
+The basis of the game is borrowed from: https://www.youtube.com/watch?v=N60lBZDEwJ8
+All credit to FamTrinli for this!
+
+Stand-up Rodel is a game build for a step-up board and a two button feet controlled custom game controller.
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#define JJ_FULLSCREEN
+
 #include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace sf;
 
 int width = 1024;
@@ -7,170 +10,285 @@ int roadW = 2000;
 int segL = 200; //segment length
 float camD = 0.84; //camera depth
 
-void drawQuad(RenderWindow &w, Color c, int x1,int y1,int w1,int x2,int y2,int w2)
+void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2, int w2)
 {
-    ConvexShape shape(4);
-    shape.setFillColor(c);
-    shape.setPoint(0, Vector2f(x1-w1,y1));
-    shape.setPoint(1, Vector2f(x2-w2,y2));
-    shape.setPoint(2, Vector2f(x2+w2,y2));
-    shape.setPoint(3, Vector2f(x1+w1,y1));
-    w.draw(shape);
+	ConvexShape shape(4);
+	shape.setFillColor(c);
+	shape.setPoint(0, Vector2f(x1 - w1, y1));
+	shape.setPoint(1, Vector2f(x2 - w2, y2));
+	shape.setPoint(2, Vector2f(x2 + w2, y2));
+	shape.setPoint(3, Vector2f(x1 + w1, y1));
+	w.draw(shape);
 }
 
 struct Line
 {
-  float x,y,z; //3d center of line
-  float X,Y,W; //screen coord
-  float curve,spriteX,clip,scale;
-  Sprite sprite;
+	float x, y, z; //3d center of line
+	float X, Y, W; //screen coord
+	float curve, spriteX, clip, scale;
+	Sprite sprite;
 
-  Line()
-  {spriteX=curve=x=y=z=0;}
+	Line()
+	{
+		spriteX = curve = x = y = z = 0;
+	}
 
-  void project(int camX,int camY,int camZ)
-  {
-    scale = camD/(z-camZ);
-    X = (1 + scale*(x - camX)) * width/2;
-    Y = (1 - scale*(y - camY)) * height/2;
-    W = scale * roadW  * width/2;
-  }
+	void project(int camX, int camY, int camZ)
+	{
+		scale = camD / (z - camZ);
+		X = (1 + scale * (x - camX)) * width / 2;
+		Y = (1 - scale * (y - camY)) * height / 2;
+		W = scale * roadW  * width / 2;
+	}
 
-  void drawSprite(RenderWindow &app)
-  {
-    Sprite s = sprite;
-    int w = s.getTextureRect().width;
-    int h = s.getTextureRect().height;
+	void drawSprite(RenderWindow &app)
+	{
+		Sprite s = sprite;
+		//int w = s.getTextureRect().width;
+		//int h = s.getTextureRect().height;
+		int w = s.getTextureRect().width;
+		int h = s.getTextureRect().height;
+		//if(w || h)std::cout << w << " " << h << std::endl;
+		//w = 256; h = 848;
 
-    float destX = X + scale * spriteX * width/2;
-    float destY = Y + 4;
-    float destW  = w * W / 266;
-    float destH  = h * W / 266;
+		float destX = X + scale * spriteX * width / 2;
+		float destY = Y + 4;
+		//float destW = w * W / 266;
+		//float destH = h * W / 266;
+		// Scale to human height
+		float destW = (w * W / 266) * 266 / w;
+		float destH = (h * W / 266) * 848 / h;
 
-    destX += destW * spriteX; //offsetX
-    destY += destH * (-1);    //offsetY
+		destX += destW * spriteX; //offsetX
+		destY += destH * (-1);    //offsetY
 
-    float clipH = destY+destH-clip;
-    if (clipH<0) clipH=0;
+		float clipH = destY + destH - clip;
+		if (clipH<0) clipH = 0;
 
-    if (clipH>=destH) return;
-    s.setTextureRect(IntRect(0,0,w,h-h*clipH/destH));
-    s.setScale(destW/w,destH/h);
-    s.setPosition(destX, destY);
-    app.draw(s);
-    }
+		if (clipH >= destH) return;
+		s.setTextureRect(IntRect(0, 0, w, h - h * clipH / destH));
+		s.setScale(destW / w, destH / h);
+		s.setPosition(destX, destY);
+		app.draw(s);
+	}
 };
+
+
+
+void getWithAndHeight(int &screen_w, int &screen_h, int &style)
+{
+#ifdef JJ_FULLSCREEN
+	// Display the list of all the video modes available for fullscreen
+	std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+	for (std::size_t i = 0; i < modes.size(); ++i)
+	{
+		sf::VideoMode mode = modes[i];
+		std::cout << "Mode #" << i << ": "
+			<< mode.width << "x" << mode.height << " - "
+			<< mode.bitsPerPixel << " bpp" << std::endl;
+	}
+
+	sf::VideoMode mode = modes[0];
+	// Create a window with the same pixel depth as the desktop
+	//sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	//sf::RenderWindow window(sf::VideoMode(mode.width, mode.height, desktop.bitsPerPixel), "SFML window", sf::Style::Fullscreen);
+	screen_w = mode.width;
+	screen_h = mode.height;
+
+	style = sf::Style::Fullscreen;
+	//#define JJ_KEEP_RATIO
+#ifdef JJ_KEEP_RATIO
+	screen_w = (screen_h * 1024) / 768;
+
+#endif
+
+#else
+	//sf::RenderWindow window(sf::VideoMode(700, 400), "Hoverboard");
+
+	screen_w = 1024;
+	screen_h = 768;
+
+	style = sf::Style::Default;
+#endif
+}
+
+
+
+
+int get_js()
+{
+	int js = 0;
+	sf::Joystick::update();
+	while (js < 8)
+	{
+		if (sf::Joystick::isConnected(js)) {
+			//printf("Joystick %i\n", js);
+			break;
+		}
+		js++;
+	}
+	if (js > 7) {
+		//printf("No joystick\n");
+	}
+	return js;
+
+}
+
+
+
 
 
 int main()
 {
-    RenderWindow app(VideoMode(width, height), "Outrun Racing!");
-    app.setFramerateLimit(60);
+	int screenStyle;
+	getWithAndHeight(width, height, screenStyle);
 
-    Texture t[50];
-    Sprite object[50];
-    for(int i=1;i<=7;i++)
-     {
-       t[i].loadFromFile("images/"+std::to_string(i)+".png");
-       t[i].setSmooth(true);
-       object[i].setTexture(t[i]);
-     }
+	RenderWindow app(VideoMode(width, height), "Outrun Racing!", screenStyle);
 
-    Texture bg;
-    bg.loadFromFile("images/bg.png");
-    bg.setRepeated(true);
-    Sprite sBackground(bg);
-    sBackground.setTextureRect(IntRect(0,0,5000,411));
-    sBackground.setPosition(-2000,0);
 
-    std::vector<Line> lines;
+	app.setFramerateLimit(60);
 
-    for(int i=0;i<1600;i++)
-     {
-       Line line;
-       line.z = i*segL;
+	Texture t[50];
+	Sprite object[50];
+	for (int i = 1; i <= 7; i++)
+	{
+		t[i].loadFromFile("images/" + std::to_string(i) + ".png");
+		t[i].setSmooth(true);
+		object[i].setTexture(t[i]);
+	}
 
-       if (i>300 && i<700) line.curve=0.5;
-       if (i>1100) line.curve=-0.7;
+	Texture bg;
+	bg.loadFromFile("images/bg.png");
+	bg.setRepeated(true);
+	Sprite sBackground(bg);
+	sBackground.setTextureRect(IntRect(0, 0, 5000, 411));
+	sBackground.setPosition(-2000, 0);
 
-       if (i<300 && i%20==0) {line.spriteX=-2.5; line.sprite=object[5];}
-       if (i%17==0)          {line.spriteX=2.0; line.sprite=object[6];}
-       if (i>300 && i%20==0) {line.spriteX=-0.7; line.sprite=object[4];}
-       if (i>800 && i%20==0) {line.spriteX=-1.2; line.sprite=object[1];}
-       if (i==400)           {line.spriteX=-1.2; line.sprite=object[7];}
+	std::vector<Line> lines;
 
-       if (i>750) line.y = sin(i/30.0)*1500;
+	for (int i = 0; i<1600; i++)
+	{
+		Line line;
+		line.z = i * segL;
 
-       lines.push_back(line);
-     }
+		if (i>300 && i<700) line.curve = 0.5;
+		if (i>1100) line.curve = -0.7;
+		/*
+		if (i<300 && i % 20 == 0) { line.spriteX = -2.5; line.sprite = object[5]; }
+		if (i % 17 == 0) { line.spriteX = 2.0; line.sprite = object[6]; }
+		if (i>300 && i % 20 == 0) { line.spriteX = -0.7; line.sprite = object[4]; }
+		if (i>800 && i % 20 == 0) { line.spriteX = -1.2; line.sprite = object[1]; }
+		if (i == 400) { line.spriteX = -1.2; line.sprite = object[7]; }
+		*/
 
-   int N = lines.size();
-   float playerX = 0;
-   int pos = 0;
-   int H = 1500;
+		if (i<300 && i % 20 == 0) { line.spriteX = -2.5; line.sprite = object[5]; }
+		if (i % 17 == 0) { line.spriteX = 3.0; line.sprite = object[6]; }
+		if (i>300 && i % 20 == 0) { line.spriteX = -2.7; line.sprite = object[4]; }
+		if (i>800 && i % 20 == 0) { line.spriteX = -4.2; line.sprite = object[1]; }
+		if (i == 400) { line.spriteX = -1.2; line.sprite = object[7]; }
 
-    while (app.isOpen())
-    {
-        Event e;
-        while (app.pollEvent(e))
-        {
-            if (e.type == Event::Closed)
-                app.close();
-        }
 
-  int speed=0;
+		if (i>750) line.y = sin(i / 30.0) * 1500;
 
-  if (Keyboard::isKeyPressed(Keyboard::Right)) playerX+=0.1;
-  if (Keyboard::isKeyPressed(Keyboard::Left)) playerX-=0.1;
-  if (Keyboard::isKeyPressed(Keyboard::Up)) speed=200;
-  if (Keyboard::isKeyPressed(Keyboard::Down)) speed=-200;
-  if (Keyboard::isKeyPressed(Keyboard::Tab)) speed*=3;
-  if (Keyboard::isKeyPressed(Keyboard::W)) H+=100;
-  if (Keyboard::isKeyPressed(Keyboard::S)) H-=100;
+		lines.push_back(line);
+	}
 
-  pos+=speed;
-  while (pos >= N*segL) pos-=N*segL;
-  while (pos < 0) pos += N*segL;
+	int N = lines.size();
+	float playerX = 0;
+	int pos = 0;
+	int H = 1500;
 
-  app.clear(Color(105,205,4));
-  app.draw(sBackground);
-  int startPos = pos/segL;
-  int camH = lines[startPos].y + H;
-  if (speed>0) sBackground.move(-lines[startPos].curve*2,0);
-  if (speed<0) sBackground.move( lines[startPos].curve*2,0);
+	int js = get_js();
 
-  int maxy = height;
-  float x=0,dx=0;
+	while (app.isOpen())
+	{
+		Event e;
+		while (app.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				app.close();
 
-  ///////draw road////////
-  for(int n = startPos; n<startPos+300; n++)  
-   {
-    Line &l = lines[n%N];
-    l.project(playerX*roadW-x, camH, startPos*segL - (n>=N?N*segL:0));
-    x+=dx;
-    dx+=l.curve;
+			if (e.key.code == sf::Keyboard::Escape)
+				app.close();
 
-    l.clip=maxy;
-    if (l.Y>=maxy) continue;
-    maxy = l.Y;
 
-    Color grass  = (n/3)%2?Color(16,200,16):Color(0,154,0);
-    Color rumble = (n/3)%2?Color(255,255,255):Color(0,0,0);
-    Color road   = (n/3)%2?Color(107,107,107):Color(105,105,105);
 
-    Line p = lines[(n-1)%N]; //previous line
+		}
 
-    drawQuad(app, grass, 0, p.Y, width, 0, l.Y, width);
-    drawQuad(app, rumble,p.X, p.Y, p.W*1.2, l.X, l.Y, l.W*1.2);
-    drawQuad(app, road,  p.X, p.Y, p.W, l.X, l.Y, l.W);
-   }
+		int speed = 0;
 
-    ////////draw objects////////
-    for(int n=startPos+300; n>startPos; n--)
-      lines[n%N].drawSprite(app);
+		if (sf::Joystick::isButtonPressed(js, 0)) playerX -= 0.1;
+		if (sf::Joystick::isButtonPressed(js, 1)) playerX += 0.1;
 
-    app.display();
-    }
+		if (Keyboard::isKeyPressed(Keyboard::Right)) playerX += 0.1;
+		if (Keyboard::isKeyPressed(Keyboard::Left)) playerX -= 0.1;
+		/*
+		if (Keyboard::isKeyPressed(Keyboard::Up)) speed = 200;
+		if (Keyboard::isKeyPressed(Keyboard::Down)) speed = -200;
+		if (Keyboard::isKeyPressed(Keyboard::Tab)) speed *= 3;
+		*/
+		speed = 200;
+		if (Keyboard::isKeyPressed(Keyboard::W)) H += 100;
+		if (Keyboard::isKeyPressed(Keyboard::S)) H -= 100;
 
-    return 0;
+		// JJ
+		if (playerX > 1.0)playerX = 1.0;
+		if (playerX < -1.0)playerX = -1.0;
+		if (playerX > 0.9 || playerX < -0.9)speed *= 0.3;
+
+		pos += speed;
+		while (pos >= N * segL) pos -= N * segL;
+		while (pos < 0) pos += N * segL;
+
+		//app.clear(Color(105, 205, 4));
+		app.clear(Color(255, 255, 255));
+		app.draw(sBackground);
+		int startPos = pos / segL;
+		int camH = lines[startPos].y + H;
+		if (speed>0) sBackground.move(-lines[startPos].curve * 2, 0);
+		if (speed<0) sBackground.move(lines[startPos].curve * 2, 0);
+
+		int maxy = height;
+		float x = 0, dx = 0;
+
+		// JJ
+		if (speed)playerX -= lines[startPos].curve * 0.022;
+
+		///////draw road////////
+		for (int n = startPos; n<startPos + 300; n++)
+		{
+			Line &l = lines[n%N];
+			l.project(playerX*roadW - x, camH, startPos*segL - (n >= N ? N * segL : 0));
+			x += dx;
+			dx += l.curve;
+
+			l.clip = maxy;
+			if (l.Y >= maxy) continue;
+			maxy = l.Y;
+			/*
+			Color grass = (n / 3) % 2 ? Color(16, 200, 16) : Color(0, 154, 0);
+			Color rumble = (n / 3) % 2 ? Color(255, 255, 255) : Color(0, 0, 0);
+			Color road = (n / 3) % 2 ? Color(107, 107, 107) : Color(105, 105, 105);
+			*/
+			Color road = (n / 3) % 2 ? Color(245, 245, 255) : Color(240, 240, 255);
+			Color rumble = (n / 3) % 2 ? Color(240, 240, 255) : Color(0, 0, 10);
+			Color grass = (n / 3) % 2 ? Color(255, 255, 255) : Color(250, 250, 255);
+
+
+
+			Line p = lines[(n - 1) % N]; //previous line
+
+			drawQuad(app, grass, 0, p.Y, width, 0, l.Y, width);
+			drawQuad(app, rumble, p.X, p.Y, p.W*1.2, l.X, l.Y, l.W*1.2);
+			drawQuad(app, road, p.X, p.Y, p.W, l.X, l.Y, l.W);
+		}
+
+		////////draw objects////////
+		for (int n = startPos + 300; n>startPos; n--)
+			lines[n%N].drawSprite(app);
+
+		app.display();
+	}
+
+	return 0;
 }
